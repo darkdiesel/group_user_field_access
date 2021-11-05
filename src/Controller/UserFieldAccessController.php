a7b2fa3bc24e88baff04caf051dfec9ac9d97afd
Igor Peshkov <igor.peshkov@gmail.com>
2021-11-05 18:09:10 +0300

    Fix some code standards.

--- src/Controller/UserFieldAccessController.php
+++ src/Controller/UserFieldAccessController.php
@@ -1,125 +1,1 @@
-<?php
-
-
-namespace Drupal\group_user_field_access\Controller;
-
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\group_user_field_access\Form\UserFieldAccessSettingsFrom;
-
-/**
- * Helper controller
- *
- * Class UserFieldAccessController
- *
- * @package Drupal\group_user_field_access\Controller
- */
-class UserFieldAccessController extends ControllerBase {
-
-  // field names that will be hidden om user edit form
-  const hidden_account_fields = [
-    'name',
-    'pass',
-    'notify',
-    'roles',
-    'status',
-  ];
-
-  const hidden_fields = [
-    'contact',
-    'timezone',
-    'language',
-  ];
-
-  const read_only_account_fields = [
-    'mail',
-  ];
-
-  const read_only_fields = [
-
-  ];
-
-  /**
-   * Check if team coordinator can edit user (that user is)
-   *
-   * @param $teamCoordinator
-   * @param $user
-   *
-   * @return bool
-   */
-  public static function teamCoordinatorCanEditUser($teamCoordinator = NULL, $user) {
-    if (!isset($teamCoordinator)) {
-      $teamCoordinator = \Drupal::currentUser();
-    }
-
-    $grp_membership_service = \Drupal::service('group.membership_loader');
-    $grps = $grp_membership_service->loadByUser($teamCoordinator);
-
-    $allow_to_edit_user = FALSE;
-
-    if ($grps) {
-      // get module settings for field access
-      $field_access_settings = UserFieldAccessSettingsFrom::getSettings();
-      $team_coordinator_group_roles = $field_access_settings->get('team_coordinator_group_roles');
-
-      foreach ($grps as $grp) {
-        $groups[] = $grp->getGroup();
-
-        $group = $grp->getGroup();
-
-        $group_type = $group->getGroupType()->id();
-
-        $team_coordinator_group = FALSE;
-
-        if (isset($team_coordinator_group_roles[$group_type]) && $team_coordinator_group_roles[$group_type]) {
-          $group_team_coordinator_role = $team_coordinator_group_roles[$group_type];
-
-          // get group member and they roles
-          $group_member = $group->getMember($teamCoordinator);
-          $group_member_roles = $group_member->getRoles();
-
-          // check if team coordinator assigned to team coordinator role
-          if (in_array($group_team_coordinator_role,
-            array_keys($group_member_roles))) {
-            $team_coordinator_group = TRUE;
-          }
-        }
-
-        // if user is team coordinator in this group
-        if ($team_coordinator_group) {
-          // check if requested user is member of team coordinator group
-          if ($group->getMember($user)) {
-            $allow_to_edit_user = TRUE;
-
-            break;
-          }
-        }
-      }
-    }
-
-    return $allow_to_edit_user;
-  }
-
-  /**
-   * Return array of user account fields
-   *
-   * @return array
-   */
-  public static function getUserAccountFileds() {
-    $fields = array_filter(
-      \Drupal::service('entity_field.manager')
-        ->getFieldDefinitions('user', 'user'),
-      function ($fieldDefinition) {
-        return $fieldDefinition instanceof \Drupal\field\FieldConfigInterface;
-      }
-    );
-
-    $_fields_options = [];
-
-    foreach ($fields as $field_name => $field_class) {
-      $_fields_options[$field_name] = $field_class->label();
-    }
-
-    return $_fields_options;
-  }
-
-}
+<?phpnamespace Drupal\group_user_field_access\Controller;use Drupal\Core\Controller\ControllerBase;use Drupal\group_user_field_access\Form\UserFieldAccessSettingsFrom;/** * Helper controller * * Class UserFieldAccessController * * @package Drupal\group_user_field_access\Controller */class UserFieldAccessController extends ControllerBase {  // field names that will be hidden om user edit form  const HIDDEN_ACCOUNT_FIELDS = [    'name',    'pass',    'notify',    'roles',    'status',  ];  const HIDDEN_FIELDS = [    'contact',    'timezone',    'language',  ];  const READ_ONLY_ACCOUNT_FIELDS = [    'mail',  ];  const READ_ONLY_FIELDS = [  ];  /**   * Check if team coordinator can edit user (that user is)   *   * @param $teamCoordinator   * @param $user   *   * @return bool   */  public static function teamCoordinatorCanEditUser($teamCoordinator = NULL, $user) {    if (!isset($teamCoordinator)) {      $teamCoordinator = \Drupal::currentUser();    }    $grp_membership_service = \Drupal::service('group.membership_loader');    $grps = $grp_membership_service->loadByUser($teamCoordinator);    $allow_to_edit_user = FALSE;    if ($grps) {      // get module settings for field access      $field_access_settings = UserFieldAccessSettingsFrom::getSettings();      $team_coordinator_group_roles = $field_access_settings->get('team_coordinator_group_roles');      foreach ($grps as $grp) {        $groups[] = $grp->getGroup();        $group = $grp->getGroup();        $group_type = $group->getGroupType()->id();        $team_coordinator_group = FALSE;        if (isset($team_coordinator_group_roles[$group_type]) && $team_coordinator_group_roles[$group_type]) {          $group_team_coordinator_role = $team_coordinator_group_roles[$group_type];          // get group member and they roles          $group_member = $group->getMember($teamCoordinator);          $group_member_roles = $group_member->getRoles();          // check if team coordinator assigned to team coordinator role          if (in_array($group_team_coordinator_role,            array_keys($group_member_roles))) {            $team_coordinator_group = TRUE;          }        }        // if user is team coordinator in this group        if ($team_coordinator_group) {          // check if requested user is member of team coordinator group          if ($group->getMember($user)) {            $allow_to_edit_user = TRUE;            break;          }        }      }    }    return $allow_to_edit_user;  }  /**   * Return array of user account fields   *   * @return array   */  public static function getUserAccountFileds() {    $fields = array_filter(      \Drupal::service('entity_field.manager')        ->getFieldDefinitions('user', 'user'),      function ($fieldDefinition) {        return $fieldDefinition instanceof \Drupal\field\FieldConfigInterface;      }    );    $_fields_options = [];    foreach ($fields as $field_name => $field_class) {      $_fields_options[$field_name] = $field_class->label();    }    return $_fields_options;  }}
--- src/Form/UserFieldAccessSettingsFrom.php
+++ src/Form/UserFieldAccessSettingsFrom.php
@@ -1,130 +1,1 @@
-<?php
-
-namespace Drupal\group_user_field_access\Form;
-
-use Drupal\Component\Utility\Environment;
-use Drupal\Core\Form\ConfigFormBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\group_user_field_access\Controller\UserFieldAccessController;
-
-/**
- * Class UserFieldAccessSettingsFrom settings form
- *
- * @package Drupal\group_user_field_access\Form
- */
-class UserFieldAccessSettingsFrom extends ConfigFormBase {
-
-  /**
-   * Config settings name.
-   *
-   * @var string
-   */
-  const SETTINGS_KEY = 'group_user_field_access.settings';
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormId() {
-    return 'group_user_field_access_settings';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function getEditableConfigNames() {
-    return [
-      static::SETTINGS_KEY,
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    // load module settings
-    $settings = $this->config(static::SETTINGS_KEY);
-
-    // Get list of user fields from /admin/config/people/accounts/fields page
-    $custom_account_fields = UserFieldAccessController::getUserAccountFileds();
-
-    $form['editable_user_fields'] = [
-      '#type' => 'checkboxes',
-      '#title' => t("Editable user fields"),
-      //'#description' => t(''),
-      '#options' => $custom_account_fields,
-      '#required' => FALSE,
-      '#default_value' => $settings->get('editable_user_fields') ?? [],
-    ];
-
-    // load group types
-    $group_types = \Drupal::entityTypeManager()
-      ->getStorage('group_type')->loadMultiple();
-
-    $form['team_coordinator_group_types'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Team coordinator group role'),
-    ];
-
-    $team_coordinator_group_roles_settings = $settings->get('team_coordinator_group_roles');
-
-    foreach ($group_types as $group_type) {
-
-      // load group roles for each group type and create select element
-      $group_roles = \Drupal::entityTypeManager()
-        ->getStorage('group_role')->loadByProperties([
-          'group_type' => $group_type->id(),
-        ]);
-
-      $_group_roles = [];
-
-      foreach ($group_roles as $group_role) {
-        $_group_roles[$group_role->id()] = $group_role->label();
-      }
-
-      $form['team_coordinator_group_types']['team_coordinator_role_group_' . $group_type->id()] = [
-        '#type' => 'select',
-        '#title' => t("@group", ['@group' => $group_type->label()]),
-        '#description' => t('Select team coordinator role for @group group type', ['@group' => $group_type->label()]),
-        '#empty_option' => t('- Select team coordinator role -'),
-        '#options' => $_group_roles,
-        '#required' => FALSE,
-        '#multiple' => FALSE,
-        '#default_value' => $team_coordinator_group_roles_settings[$group_type->id()] ?? '',
-      ];
-    }
-
-    return parent::buildForm($form, $form_state);
-  }
-
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $team_coordinator_group_roles = [];
-
-    $group_types = \Drupal::entityTypeManager()
-      ->getStorage('group_type')->loadMultiple();
-
-    foreach ($group_types as $group_type) {
-      $form_state->getValue('team_coordinator_role_group_' . $group_type->id());
-
-      $team_coordinator_group_roles[$group_type->id()] = $form_state->getValue('team_coordinator_role_group_' . $group_type->id());
-    }
-
-    $this->configFactory->getEditable(static::SETTINGS_KEY)
-      ->set('editable_user_fields', $form_state->getValue('editable_user_fields'))
-      ->set('team_coordinator_group_roles', $team_coordinator_group_roles)
-      ->save();
-
-    parent::submitForm($form, $form_state);
-  }
-
-  public static function getSettings() {
-    return \Drupal::config(self::SETTINGS_KEY);
-  }
-
-}
+<?phpnamespace Drupal\group_user_field_access\Form;use Drupal\Component\Utility\Environment;use Drupal\Core\Form\ConfigFormBase;use Drupal\Core\Form\FormStateInterface;use Drupal\group_user_field_access\Controller\UserFieldAccessController;/** * Class UserFieldAccessSettingsFrom settings form * * @package Drupal\group_user_field_access\Form */class UserFieldAccessSettingsFrom extends ConfigFormBase {  /**   * Config settings name.   *   * @var string   */  const SETTINGS_KEY = 'group_user_field_access.settings';  /**   * {@inheritdoc}   */  public function getFormId() {    return 'group_user_field_access_settings';  }  /**   * {@inheritdoc}   */  protected function getEditableConfigNames() {    return [      static::SETTINGS_KEY,    ];  }  /**   * {@inheritdoc}   */  public function buildForm(array $form, FormStateInterface $form_state) {    // load module settings    $settings = $this->config(static::SETTINGS_KEY);    // Get list of user fields from /admin/config/people/accounts/fields page    $custom_account_fields = UserFieldAccessController::getUserAccountFileds();    $form['editable_user_fields'] = [      '#type' => 'checkboxes',      '#title' => t("Editable user fields"),      //'#description' => t(''),      '#options' => $custom_account_fields,      '#required' => FALSE,      '#default_value' => $settings->get('editable_user_fields') ?? [],    ];    // load group types    $group_types = \Drupal::entityTypeManager()      ->getStorage('group_type')->loadMultiple();    $form['team_coordinator_group_types'] = [      '#type' => 'fieldset',      '#title' => $this->t('Team coordinator group role'),    ];    $team_coordinator_group_roles_settings = $settings->get('team_coordinator_group_roles');    foreach ($group_types as $group_type) {      // load group roles for each group type and create select element      $group_roles = \Drupal::entityTypeManager()        ->getStorage('group_role')->loadByProperties([          'group_type' => $group_type->id(),        ]);      $_group_roles = [];      foreach ($group_roles as $group_role) {        $_group_roles[$group_role->id()] = $group_role->label();      }      $form['team_coordinator_group_types']['team_coordinator_role_group_' . $group_type->id()] = [        '#type' => 'select',        '#title' => t("@group", ['@group' => $group_type->label()]),        '#description' => t('Select team coordinator role for @group group type', ['@group' => $group_type->label()]),        '#empty_option' => t('- Select team coordinator role -'),        '#options' => $_group_roles,        '#required' => FALSE,        '#multiple' => FALSE,        '#default_value' => $team_coordinator_group_roles_settings[$group_type->id()] ?? '',      ];    }    return parent::buildForm($form, $form_state);  }  public function validateForm(array &$form, FormStateInterface $form_state) {    parent::validateForm($form, $form_state);  }  /**   * {@inheritdoc}   */  public function submitForm(array &$form, FormStateInterface $form_state) {    $team_coordinator_group_roles = [];    $group_types = \Drupal::entityTypeManager()      ->getStorage('group_type')->loadMultiple();    foreach ($group_types as $group_type) {      $form_state->getValue('team_coordinator_role_group_' . $group_type->id());      $team_coordinator_group_roles[$group_type->id()] = $form_state->getValue('team_coordinator_role_group_' . $group_type->id());    }    $this->configFactory->getEditable(static::SETTINGS_KEY)      ->set('editable_user_fields', $form_state->getValue('editable_user_fields'))      ->set('team_coordinator_group_roles', $team_coordinator_group_roles)      ->save();    parent::submitForm($form, $form_state);  }  public static function getSettings() {    return \Drupal::config(self::SETTINGS_KEY);  }}
